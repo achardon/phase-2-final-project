7e2e1f1d43684cad50dfe6a969e83584ad030b31
Alessandra Chardon <achardon@ucla.edu>
2021-11-22 13:17:52 -0500

    test with json deployment

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,7 +11,7 @@ function App() {
   const history = useHistory();
   
   useEffect(() => {
-    fetch("http://localhost:3000/players")
+    fetch("https://tranquil-cliffs-49188.herokuapp.com/players")
       .then((r) => r.json())
       .then((data) => {
         setPlayers(data);
