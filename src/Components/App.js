745501a313d3f66b6c15de8dea727e389030e019
Alessandra Chardon <achardon@ucla.edu>
2021-11-23 10:11:49 -0500

    json for add player only

--- src/Components/App.js
+++ src/Components/App.js
@@ -19,7 +19,7 @@ function App() {
   }, []);
 
   function addNewPlayer(newPlayer) {
-    fetch(`http://localhost:3000/players`, {
+    fetch(`https://tranquil-cliffs-49188.herokuapp.com/players`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
